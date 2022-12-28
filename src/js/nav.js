0a90b3619e7bc6eee5dd45b59c8fa30a20a8e25e
nethmi melisha <nethmimelisha.jayalath@gmail.com>
2022-12-28 20:08:51 +0530

    about us

--- src/js/nav.js
+++ src/js/nav.js
@@ -42,7 +42,7 @@ nav.innerHTML = nav_html;
 // Footer HTML
 
 const footer_html =`
-            <p>Copyright © 2020 Parachute Sri Lanka | parachutesrilanka@gmail.com </p>
+            <p>Copyright © 2022 Parachute Sri Lanka | parachutesrilanka@gmail.com </p>
 `;
 
 // Insert the HTML into the DOM section.footer
