c756cd8e222cc4ff51292347459b20e55fd92e93
Nushan Kodikara <nushankodi@gmail.com>
2022-12-29 23:25:15 +0530

    added Deployment requirements

--- src/server.php
+++ src/server.php
@@ -1,8 +1,15 @@
 <?php
 
-$server = "mysql";
-$username = "root";
-$password = "root";
-$dbname = "parachutesrilanka";
+// Production
+$server = "localhost";
+$username = "parachut_user";
+$password = "o2x!.HpC0vz=";
+$dbname = "parachut_parachutesrilanka";
+
+// Development
+// $server = "mysql";
+// $username = "root";
+// $password = "root";
+// $dbname = "parachutesrilanka";
 
 ?>
